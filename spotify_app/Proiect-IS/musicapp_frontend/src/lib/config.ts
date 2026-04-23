6505a245f1d0184db7674a722671e99e32359462
Calacean Titus <62383494+Titus12-code9823@users.noreply.github.com>
2026-04-23 19:50:12 +0300

    Delete spotify_app/Proiect-IS/musicapp_frontend directory

--- spotify_app/Proiect-IS/musicapp_frontend/src/lib/config.ts
+++
@@ -1,7 +0,0 @@
-const API_BASE_URL =
-  import.meta.env.VITE_API_URL?.replace(/\/$/, '') ?? 'http://localhost:8080';
-
-export const appConfig = {
-  apiBaseUrl: API_BASE_URL
-};
-
--- spotify_app/Proiect-IS/musicapp_frontend/src/services/artistService.ts
+++
@@ -1,8 +0,0 @@
-import { apiClient } from './apiClient';
-import { Artist } from '../types/music';
-
-const BASE_PATH = '/artists';
-
-export const fetchArtists = () =>
-  apiClient.get<Artist[]>(BASE_PATH, { auth: true });
-
